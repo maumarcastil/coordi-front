3ae816495239ea4ec5eb6451937eb67458ac97ce
Mauricio Martinez <maumarcastil@gmail.com>
2025-12-19 17:41:37 -0500

    feat: add login schema and types for authentication
    - Introduce loginSchema using zod for form validation
    - Update auth types to include LoginFormData for login functionality

--- features/auth/schemas/register.schema.ts
+++ features/auth/schemas/register.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const registerSchema = z
 	.object({
 		name: z.string().min(2, "El nombre debe tener al menos 2 caracteres"),
-		email: z.string().email("Email inválido"),
+		email: z.email("Email inválido"),
 		password: z
 			.string()
 			.min(6, "La contraseña debe tener al menos 6 caracteres"),
--- features/auth/types/auth.types.ts
+++ features/auth/types/auth.types.ts
@@ -1,8 +1,11 @@
 import type { z } from "zod";
 
+import type { loginSchema } from "@/features/auth/schemas/login.schema";
 import type { registerSchema } from "@/features/auth/schemas/register.schema";
 
 export type RegisterFormData = z.infer<typeof registerSchema>;
+
+export type LoginFormData = z.infer<typeof loginSchema>;
 
 export type RegisterRequest = Omit<RegisterFormData, "confirmPassword">;
 
